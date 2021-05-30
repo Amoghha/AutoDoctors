d3a3d84d94376b5e4382ce62f247344045db2844
Amogha Raut <amogha.raut@gmail.com>
2021-05-30 09:14:46 +0545

    initial project status

---
+++ src/main/java/com/WinOrbooze/AutoDoctors/AutoDoctorsApplication.java
@@ -0,0 +1,13 @@
+package com.WinOrbooze.AutoDoctors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoDoctorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoDoctorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WinOrbooze/AutoDoctors/AutoDoctorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WinOrbooze.AutoDoctors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoDoctorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
